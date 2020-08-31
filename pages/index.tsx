84dcd1a214befd49d0bf8653c177eb15e1249f51
César Augusto Costa <cezarcozta@gmail.com>
2020-08-31 19:08:02 -0300

    linting

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>ON / OFF</title>
+      </Head>
+
+      <main>
+        <h1>ON / OFF</h1>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
